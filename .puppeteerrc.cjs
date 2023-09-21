5d7f53f34a33d718e1eb09297e50878f40ba36d5
Gabriel Alabi <gabriel.alabi@tiarg.com.ar>
2023-09-21 01:41:34 -0300

    trying to fix scrapp

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,12 @@
+const { join } = require("path");
+
+console.log("Puppeteer config loaded");
+console.log(
+  "Puppeteer cache directory:",
+  join(__dirname, ".cache", "puppeteer")
+);
+
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+};
--- src/pages/api/housing-results-data.js
+++ src/pages/api/housing-results-data.js
@@ -12,6 +12,11 @@ async function chooseRandomUrlAndFetch() {
   const urlToFetch = `https://www.zonaprop.com.ar/departamentos-alquiler-${randomLocation}-orden-publicado-descendente-pagina-${randomPage}.html`;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  const client = await page.target().createCDPSession();
+  await client.send("Page.setDownloadBehavior", {
+    behavior: "allow",
+    downloadPath: "./Downloads",
+  });
   await page.setViewport({ width: 1920, height: 1080 });
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
@@ -25,81 +30,86 @@ async function chooseRandomUrlAndFetch() {
   return elements;
 }
 export default async function handler(req, res) {
-  let elements = await chooseRandomUrlAndFetch();
-  const apartmentData = {};
-  let element, matches, uniqueUrls, priceMatch, metersMatch;
-  let arrayOfImages = new Set();
-  const maxRetries = 10;
-  let retries = 0;
-  console.log("entro");
-  do {
-    if (retries >= maxRetries) {
-      elements = await chooseRandomUrlAndFetch();
-      retries = 0;
-    }
-    element = chooseRandomElement(elements);
-    matches = element.match(/src="([^"]+)"/g);
-    priceMatch = element.match(
-      /<div data-qa="POSTING_CARD_PRICE"[^>]*>(.*?)<\/div>/
+  try {
+    let elements = await chooseRandomUrlAndFetch();
+    const apartmentData = {};
+    let element, matches, uniqueUrls, priceMatch, metersMatch;
+    let arrayOfImages = new Set();
+    const maxRetries = 10;
+    let retries = 0;
+    console.log("entro");
+
+    do {
+      if (retries >= maxRetries) {
+        elements = await chooseRandomUrlAndFetch();
+        retries = 0;
+      }
+      element = chooseRandomElement(elements);
+      matches = element.match(/src="([^"]+)"/g);
+      priceMatch = element.match(
+        /<div data-qa="POSTING_CARD_PRICE"[^>]*>(.*?)<\/div>/
+      );
+      uniqueUrls = new Set(
+        matches
+          .map((match) => match.match(/src="([^"]+)"/)[1])
+          .filter(
+            (url) =>
+              !url.includes("Sprite") &&
+              !url.includes("logo") &&
+              !url.includes("placeholder") &&
+              !url.includes("empresas") &&
+              !url.includes(".svg") &&
+              !url.includes(".png")
+          )
+      );
+
+      uniqueUrls.forEach((url) => {
+        arrayOfImages.add(url); // Add unique URLs to the set
+      });
+      metersMatch = element.match(/(\d+)\s*m²/);
+      if (metersMatch) {
+        apartmentData.meters = `${metersMatch[1].trim()} mts.`;
+      }
+      retries++;
+    } while (
+      arrayOfImages.size < 3 ||
+      priceMatch[1].trim().includes("Consultar") ||
+      parseInt(metersMatch[1].trim()) < 15
     );
-    uniqueUrls = new Set(
-      matches
-        .map((match) => match.match(/src="([^"]+)"/)[1])
-        .filter(
-          (url) =>
-            !url.includes("Sprite") &&
-            !url.includes("logo") &&
-            !url.includes("placeholder") &&
-            !url.includes("empresas") &&
-            !url.includes(".svg") &&
-            !url.includes(".png")
-        )
+    apartmentData.arrayOfImages = Array.from(arrayOfImages);
+
+    if (priceMatch) {
+      if (element.includes("USD") || priceMatch < 5000) {
+        apartmentData.prizeInUSD = `${priceMatch[1].trim()}`;
+        const dolarBlueValues = await fetch(
+          "https://api.bluelytics.com.ar/v2/latest"
+        ).then((data) => data.json());
+        const { value_sell } = dolarBlueValues.blue;
+        apartmentData.prizeInARS = `ARS ${
+          parseInt(
+            priceMatch[1].trim().replace("USD ", "").replace(/\./g, "")
+          ) * parseInt(value_sell)
+        }`;
+        console.log("prize in ars", apartmentData.prizeInARS);
+      } else {
+        apartmentData.prizeInARS = `ARS ${priceMatch[1].trim()}`;
+      }
+    }
+
+    const locationMatch = element.match(
+      /<div data-qa="POSTING_CARD_LOCATION"[^>]*>(.*?)<\/div>/
     );
-
-    uniqueUrls.forEach((url) => {
-      arrayOfImages.add(url); // Add unique URLs to the set
-    });
-    metersMatch = element.match(/(\d+)\s*m²/);
-    if (metersMatch) {
-      apartmentData.meters = `${metersMatch[1].trim()} mts.`;
+    if (locationMatch) {
+      apartmentData.location = locationMatch[1].trim();
     }
-    retries++;
-  } while (
-    arrayOfImages.size < 3 ||
-    priceMatch[1].trim().includes("Consultar") ||
-    parseInt(metersMatch[1].trim()) < 15
-  );
-  apartmentData.arrayOfImages = Array.from(arrayOfImages);
-
-  if (priceMatch) {
-    if (element.includes("USD") || priceMatch < 5000) {
-      apartmentData.prizeInUSD = `${priceMatch[1].trim()}`;
-      const dolarBlueValues = await fetch(
-        "https://api.bluelytics.com.ar/v2/latest"
-      ).then((data) => data.json());
-      const { value_sell } = dolarBlueValues.blue;
-      apartmentData.prizeInARS = `ARS ${
-        parseInt(priceMatch[1].trim().replace("USD ", "").replace(/\./g, "")) *
-        parseInt(value_sell)
-      }`;
-      console.log("prize in ars", apartmentData.prizeInARS);
-    } else {
-      apartmentData.prizeInARS = `ARS ${priceMatch[1].trim()}`;
+    const descriptionMatch = element.match(
+      /<div data-qa="POSTING_CARD_DESCRIPTION"[^>]*>(.*?)<\/div>/
+    );
+    if (descriptionMatch) {
+      apartmentData.description = descriptionMatch[1].trim();
     }
+    res.status(200).json(apartmentData);
+  } catch (error) {
+    console.log("apartmentDataError", error);
   }
-
-  const locationMatch = element.match(
-    /<div data-qa="POSTING_CARD_LOCATION"[^>]*>(.*?)<\/div>/
-  );
-  if (locationMatch) {
-    apartmentData.location = locationMatch[1].trim();
-  }
-  const descriptionMatch = element.match(
-    /<div data-qa="POSTING_CARD_DESCRIPTION"[^>]*>(.*?)<\/div>/
-  );
-  if (descriptionMatch) {
-    apartmentData.description = descriptionMatch[1].trim();
-  }
-  console.log("apartmentData", apartmentData);
-  res.status(200).json(apartmentData);
 }

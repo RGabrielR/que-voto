cd7118c8b17f63eaef8b59623f0a4c6c01c87915
Gabriel Alabi <gabriel.alabi@tiarg.com.ar>
2023-09-28 18:25:11 -0300

    se agrega console log

--- src/pages/api/housing-results-data.js
+++ src/pages/api/housing-results-data.js
@@ -33,6 +33,7 @@ async function chooseRandomUrlAndFetch() {
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
   );
   console.log("llega aca???");
+  console.log("la url esta bien? (antes del goto)", urlToFetch);
   await page.goto(urlToFetch);
   console.log("esta en el goto?");
   console.log("la url esta bien?", urlToFetch);
